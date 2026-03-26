48da7bbfc08176af0293e451e3b180a71c098cc8
Jake Soenneker <jake@soenneker.com>
2026-03-26 17:51:14 +0000

    Automated update

--- src/Soenneker.Persona.OpenApiClient/Models/Cases_results_metadata.cs
+++ src/Soenneker.Persona.OpenApiClient/Models/Cases_results_metadata.cs
@@ -7,29 +7,30 @@ using System.IO;
 using System;
 namespace Soenneker.Persona.OpenApiClient.Models
 {
+    /// <summary>
+    /// This is a free-form object containing potentially helpful metadata on the extraction result.
+    /// </summary>
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
-    #pragma warning disable CS1591
-    public partial class Inquiries_structuredResults : IAdditionalDataHolder, IParsable
-    #pragma warning restore CS1591
+    public partial class Cases_results_metadata : IAdditionalDataHolder, IParsable
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.Persona.OpenApiClient.Models.Inquiries_structuredResults"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.Persona.OpenApiClient.Models.Cases_results_metadata"/> and sets the default values.
         /// </summary>
-        public Inquiries_structuredResults()
+        public Cases_results_metadata()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Persona.OpenApiClient.Models.Inquiries_structuredResults"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Persona.OpenApiClient.Models.Cases_results_metadata"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.Persona.OpenApiClient.Models.Inquiries_structuredResults CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.Persona.OpenApiClient.Models.Cases_results_metadata CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Persona.OpenApiClient.Models.Inquiries_structuredResults();
+            return new global::Soenneker.Persona.OpenApiClient.Models.Cases_results_metadata();
         }
         /// <summary>
         /// The deserialization information for the current model
